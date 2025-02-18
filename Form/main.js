7a50322c3faac2ccac018f42165d06cc8a703091
24CSB0B46-Saket-Modi <sm24csb0b46@student.nitw.ac.in>
2025-02-18 22:44:17 +0530

    Update main.js

--- Form/main.js
+++ Form/main.js
@@ -1,12 +1,28 @@
-function makeSure(x)
+function makeSure(x) 
 {
     let confirmation;
-    if (x == 1)
+    const phoneInput = document.getElementById('phone').value;
+    const phoneRegex = /^\d{10}$/;
+
+    if (x == 1) 
     {
+        if (!phoneRegex.test(phoneInput)) {
+            alert("Please enter a valid phone number with exactly 10 digits.");
+            return false;
+        }
         confirmation = confirm("Are you sure you wish to submit?");
-        if (confirmation == true)
+        if (confirmation == true) 
+        {
             alert("Form submitted!");
+            return true;
+        } 
+        else
+            return false;
+
     }    
-    if (x == 2)
+    if (x == 2) 
+    {
         confirmation = confirm("Are you sure you wish to reset?");
+        return true; // Allow reset
+    }
 }
